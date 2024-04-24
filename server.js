03f45f36ee86bb2ece81ec39bce775658fe2ffd1
minhhieudev <minhhieudev31@gmail.com>
2024-04-24 13:45:40 +0700

    UP

--- server.js
+++ server.js
@@ -81,9 +81,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 
 
 // connect to mongo
