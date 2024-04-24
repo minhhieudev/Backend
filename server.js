9a1b52c875d6a277f463b1309972c3d68fb12f70
minhhieudev <minhhieudev31@gmail.com>
2024-04-24 10:44:35 +0700

    upcode

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose')
+mongoose.set('strictQuery', false);
 const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
 const cors = require("cors");
