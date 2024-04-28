a279425f60f46a1a0203d463799d3852acb22890
minhhieudev <minhhieudev31@gmail.com>
2024-04-28 21:23:27 +0700

    up

--- app/routes/admin/search.js
+++ app/routes/admin/search.js
@@ -1,77 +1,89 @@
 const express = require("express");
 const router = express.Router();
-const { Builder, By, until } = require('selenium-webdriver');
+const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
-router.get("/resultLearning/:id", async (req, res) => {
-    const idSinhVien = req.params.id; // Lấy id từ đường dẫn
-    const URL = `http://dangkymonhoc.pyu.edu.vn/Default.aspx?page=xemdiemthi&id=${idSinhVien}`;
+// Khởi động trình duyệt Puppeteer một lần và tái sử dụng nó cho mỗi yêu cầu
+let browserInstance;
 
-    // Khởi tạo trình duyệt
-    const driver = new Builder().forBrowser('chrome').build();
+router.get(
+    "/resultLearning/:id",
+    async (req, res) => {
+        try {
+            const idSinhVien = req.params.id; // Lấy id từ đường dẫn
+            const URL = `http://dangkymonhoc.pyu.edu.vn/Default.aspx?page=xemdiemthi&id=${idSinhVien}`;
 
-    try {
-        // Truy cập URL
-        await driver.get(URL);
+            // Kiểm tra xem trình duyệt đã được khởi động chưa
+            if (!browserInstance) {
+                browserInstance = await puppeteer.launch({
+                    headless: 'new',
+                    args: ['--no-sandbox', '--disable-setuid-sandbox']
+                });
+            }
 
-        // Chờ cho liên kết được tải và nhấp vào nó
-        const linkSelector = By.css('#ctl00_ContentPlaceHolder1_ctl00_lnkChangeview2');
-        await driver.wait(until.elementLocated(linkSelector), 10000);
-        const linkElement = await driver.findElement(linkSelector);
-        await linkElement.click();
+            // Tạo một trang mới từ trình duyệt đã khởi động trước đó
+            const page = await browserInstance.newPage();
 
-        // Chờ điều hướng sau khi nhấp vào liên kết
-        await driver.wait(until.urlContains('dangkymonhoc.pyu.edu.vn'), 10000);
+            // Truy cập trang web
+            await page.goto(URL);
 
-        // Lấy nội dung HTML sau khi nhấp vào liên kết
-        const htmlContent = await driver.getPageSource();
+            // Chờ cho liên kết được tải và sau đó nhấp vào nó
+            await page.waitForSelector('#ctl00_ContentPlaceHolder1_ctl00_lnkChangeview2');
+            await page.click('#ctl00_ContentPlaceHolder1_ctl00_lnkChangeview2');
 
-        // Sử dụng Cheerio để phân tích HTML
-        const $ = cheerio.load(htmlContent);
+            // Chờ cho trang được tải sau khi nhấp vào liên kết
+            await page.waitForNavigation();
 
-        const kyvanamArray = [];
+            // Lấy nội dung HTML sau khi nhấp vào liên kết
+            const htmlContent = await page.content();
 
-        // Tìm và lưu thông tin về học kỳ và năm học vào mảng
-        $('.title-hk-diem').each((index, element) => {
-            // Trích xuất thông tin về học kỳ và năm học
-            const label1 = $(element).find('.Label').eq(0).text().trim();
-            const kyvanamObject = {
-                kyandnam: label1,
-                data: []
-            };
+            // Đóng trang sau mỗi yêu cầu
+            await page.close();
 
-            // Đẩy đối tượng mới vào mảng
-            kyvanamArray.push(kyvanamObject);
-        });
+            // Sử dụng Cheerio để phân tích HTML
+            const $ = cheerio.load(htmlContent);
 
-        // Tìm và lưu thông tin điểm trung bình học kỳ hệ 4 vào mảng mới
-        $('.row-diemTK').each((index, element) => {
-            // Trích xuất thông tin về điểm trung bình học kỳ
-            const label = $(element).find('.Label').eq(1).text().trim();
-            const score = $(element).find('.Label').eq(0).text().trim();
+            const kyvanamArray = [];
 
-            // Lấy thông tin học kỳ và năm học tương ứng
-            const kyvanamIndex = Math.floor(index / 6);
+            // Tìm và lưu thông tin về học kỳ và năm học vào mảng
+            $('.title-hk-diem').each((index, element) => {
+                // Trích xuất thông tin về học kỳ và năm học
+                const label1 = $(element).find('.Label').eq(0).text().trim();
+                const kyvanamObject = {
+                    kyandnam: label1,
+                    data: []
+                };
 
-            // Tạo một đối tượng mới chứa thông tin label và score
-            const diemTrungBinhHKObject = {
-                label,
-                score
-            };
+                // Đẩy đối tượng mới vào mảng
+                kyvanamArray.push(kyvanamObject);
+            });
 
-            // Đẩy đối tượng mới vào mảng data trong đối tượng tương ứng trong kyvanamArray
-            kyvanamArray[kyvanamIndex].data.push(diemTrungBinhHKObject);
-        });
+            // Tìm và lưu thông tin điểm trung bình học kỳ hệ 4 vào mảng mới
+            $('.row-diemTK').each((index, element) => {
+                // Trích xuất thông tin về điểm trung bình học kỳ
+                const label = $(element).find('.Label').eq(0).text().trim();
+                const score = $(element).find('.Label').eq(1).text().trim();
 
-        // Gửi dữ liệu JSON về client
-        res.json(kyvanamArray);
-    } catch (error) {
-        console.error('Error fetching and parsing data:', error);
-        res.status(500).send('Error fetching and parsing data');
-    } finally {
-        // Đóng trình duyệt sau khi hoàn thành
-        await driver.quit();
+                // Lấy thông tin học kỳ và năm học tương ứng
+                const kyvanamIndex = Math.floor(index / 6); // Mỗi học kỳ có 6 thông tin điểm, chia lấy phần nguyên để xác định index trong mảng kyvanamArray
+
+                // Tạo một đối tượng mới chứa thông tin label và score
+                const diemTrungBinhHKObject = {
+                    label: label,
+                    score: score
+                };
+
+                // Đẩy đối tượng mới vào mảng data trong đối tượng tương ứng trong kyvanamArray
+                kyvanamArray[kyvanamIndex].data.push(diemTrungBinhHKObject);
+            });
+
+            // Gửi nội dung HTML về client
+            res.json(kyvanamArray);
+        } catch (error) {
+            console.error('Error fetching and parsing data:', error);
+            res.status(500).send('Error fetching and parsing data');
+        }
     }
-});
+);
 
 module.exports = router;
